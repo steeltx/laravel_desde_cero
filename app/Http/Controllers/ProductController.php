6809196439996f44160d2a8b4220ffaa03ec0c99
steeltx <realsteel.gnu@gmail.com>
2022-03-20 19:24:04 -0600

    Factory y consultar datos con Query Builder

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function index(){
+
+        $products = Product::all();
+        //$products = DB::table('products')->get();
+
         return view('products.index');
     }
 
@@ -19,7 +25,12 @@ class ProductController extends Controller
     }
 
     public function show($product){
+
+        $product = Product::findOrFail($product);
+        //$product = DB::table('products')->find($product);
+
         return view('products.show');
+
     }
 
     public function edit($product){
