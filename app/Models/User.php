adeb568cbffa3e19a3921501e6741f8b0ac711d9
steeltx <realsteel.gnu@gmail.com>
2022-03-22 23:53:25 -0600

    Ajustes en modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        //'admin_since'
     ];
 
     /**
@@ -40,5 +41,9 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+    ];
+
+    protected $dates = [
+        'admin_since',
     ];
 }
