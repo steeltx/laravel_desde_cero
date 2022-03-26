8ddb76d3aba7b4b608e9d91d2ec040e59721ce4c
steeltx <realsteel.gnu@gmail.com>
2022-03-25 23:53:17 -0600

    Relaciones polimórficas 1 a muchos y muchos a muchos

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,12 +24,17 @@ class Product extends Model
 
     public function carts()
     {
-        return $this->belongsToMany(Cart::class)->withPivot('quantity');
+        return $this->morphedByMany(Cart::class, 'productable')->withPivot('quantity');
     }
 
     public function orders()
     {
-        return $this->belongsToMany(Order::class)->withPivot('quantity');
+        return $this->morphedByMany(Order::class, 'productable')->withPivot('quantity');
+    }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
     }
 
 }
--- database/migrations/2022_03_26_035316_create_productables_table.php
+++ database/migrations/2022_03_26_035316_create_productables_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderProductTable extends Migration
+class CreateProductablesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateOrderProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('order_product', function (Blueprint $table) {
-            $table->bigInteger('order_id')->unsigned();
+        Schema::create('productables', function (Blueprint $table) {
+            //$table->bigInteger('order_id')->unsigned();
             $table->bigInteger('product_id')->unsigned();
             $table->integer('quantity')->unsigned();
 
+            $table->morphs('productable');
+
             // agregar las llaves foraneas
-            $table->foreign('order_id')->references('id')->on('orders');
+            //$table->foreign('order_id')->references('id')->on('orders');
             $table->foreign('product_id')->references('id')->on('products');
         });
     }
